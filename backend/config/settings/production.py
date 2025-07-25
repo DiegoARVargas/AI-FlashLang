232eecdd9e38785038e4266456fa3df109f8e219
diego <rodriguezvargasd214@gmail.com>
2025-07-24 22:41:38 -0700

    Se agrega CSRF_TRUSTED_ORIGINS a config.settings.production

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -16,3 +16,4 @@ SECURE_SSL_REDIRECT = os.getenv("SECURE_SSL_REDIRECT", "True") == "True"
 # Asegura que las cookies sólo se transmitan por HTTPS
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
+CSRF_TRUSTED_ORIGINS = ["https://ai-flashlang-backend.onrender.com"]
